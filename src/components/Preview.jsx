4ea9d3a0232a3768be1f65853048f431caed052a
Arrasyd Nanda Fairuz - AI Fullstack Developer <lexxzy112@gmail.com>
2026-06-16 21:18:08 +0700

    Fix id from 'grup' to 'group' and add new group

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -11,12 +11,20 @@ const LINKS = [
     url: 'https://chat.whatsapp.com/JemTjOh5D4N8mTEsEFm0Hf?mode=gi_t'
   },
   {
-    id: 'grup',
+    id: 'group',
     title: 'Group WhatsApp 2',
     description: 'Klik untuk menggunakan dengan free!',
     iconUrl: 'https://i.ibb.co.com/DXd8RV7/download-4.jpg', // Example placeholder URL
     url: 'https://chat.whatsapp.com/EypWtqf9uke8s1z6uynLxD'
   },
+  {
+    id: 'group',
+    title: 'Group WhatsApp 3',
+    description: 'Klik untuk menggunakan dengan free!',
+    iconUrl: 'https://i.ibb.co.com/DXd8RV7/download-4.jpg', // Example placeholder URL
+    url: 'https://chat.whatsapp.com/E3zRaurdyGc2wevlHwslkm'
+  },
+
   {
     id: 'channel',
     title: 'Channel WhatsApp',
